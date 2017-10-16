294515b641c05c9c20e970eb68a3cd618cd20f73
Chengyu <1529514910@qq.com>
2017-10-15 21:25:31 -0700

    MongoDB connects with front-end code

--- connect_server.js
+++ connect_server.js
@@ -1,14 +1,40 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/mydb";
+var url = "mongodb://localhost:27017/spotlite";
 
-// insertItem();
-findItem();
+
+var app, express;
+
+express = require("express");
+
+app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+
+//reponse after receiving the request from front end via "/insertElement".
+app.post("/insertElement", function (request, response) {
+    console.log(request.body);
+    MongoClient.connect(url, function(err,db) {
+        if (err) throw err;
+        var myobj = {email : request.body.email, name: request.body.name, phone : request.body.phone};
+        db.collection("users").insertOne(myobj, function(err,res) {
+            if (err) throw err;
+            console.log("Insert 1 element correctly");
+            db.close();
+        });
+    });
+    response.send("saved");
+});
+
+app.listen(8888);
+
 
 //create a collection/another word for table
-function createTable(){
+function createCollection(TableName){
     MongoClient.connect(url, function(err, db) {
       if (err) throw err;
-      db.createCollection("users", function(err, res) {
+      db.createCollection(TableName, function(err, res) {
         if (err) throw err;
         console.log("create users table");
         db.close();
@@ -17,10 +43,10 @@ function createTable(){
 }
 
 //Insert one attribute to a collection
-function insertItem(){
-    MongoClient.connect(url, function(err,db){
-        if (err) throw err;
-        var myobj = {name : "Alex", address : "Ellendale"};
+function insertItem(username, password, nickname, email, phone, starttime, score, paired, pictureconfirmed, invitation){
+    MongoClient.connect(url, function(err,db) {
+       if (err) throw err;
+        var myobj = {username : username, password: password, nickname : nickname, email : email, phone : phone, starttime: starttime, score: score, paired: paired, pictureconfirmed: pictureconfirmed, invitation: invitation };
         db.collection("users").insertOne(myobj, function(err,res) {
             if (err) throw err;
             console.log("Insert 1 element correctly");
@@ -30,13 +56,26 @@ function insertItem(){
 }
 
 //find one attribute from a collection
-function findItem(){
+function findItem(username){
     MongoClient.connect(url, function(err, db) {
       if (err) throw err;
-      db.collection("users").findOne({}, function(err, result) {
+      db.collection("users").findOne({username : username}, function(err, result) {
         if (err) throw err;
         console.log(result);
         db.close();
       });
+    });
+}
+
+//search the required elements
+function search(name){
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var query = {name : name};
+        db.collection("users").find(query).toArray(function(err, result) {
+           if (err) throw err;
+            console.log(result);
+            db.close();
+        });
     });
 }
