6446d4bf592be9861282896349728ce65622d0c8
Chengyu <1529514910@qq.com>
2017-10-20 14:16:21 -0700

    JIRA ticket: POP-81 - Implement backend apis which were promoted in 10/17/2017

--- connect_server.js
+++ connect_server.js
@@ -13,27 +13,55 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 
-findItem("aiodjoijd");
+// findItem("aiodjoijd");
 
-//save one user".
-app.post("/insertElement", function (request, response) {
-    MongoClient.connect(url, function(err,db) {
-        if (err) throw err;
-        var myobj = {email : request.body.email, name: request.body.name, phone : request.body.phone};
-        db.collection("users").insertOne(myobj, function(err,res) {
-            if (err) throw err;
-            console.log("Insert 1 element correctly");
-            db.close();
-        });
-    });
-    response.send("saved");
+//1. save one user.
+app.post("/insertUser", function (request, response) {
+    var result = insertItem(request.body.username, request.body.password, request.body.nickname, request.body.email, request.body.phone, request.body.score);
+    response.send(result);
 });
 
-//The front end want to get a user information".
+//2. The front end want to get a user information.
+app.post("/getChallengers", function (request, response) {
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        db.collection("Challenger").find({challengee : request.body.username}).toArray(function(err, result) {
+            if (err) throw err;
+            if(result == null){
+                console.log("this is null");
+            }
+            else{
+                console.log("find and fetch some items...");
+            }
+            db.close();
+            response.send(result);
+        });
+    });
+});
+
+//3. The front end want to get a user information.
+app.post("/getCompetitor", function (request, response) {
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        db.collection("Challenger").findOne({challengee : request.body.username, isMatched: true}, function(err, result) {
+            if (err) throw err;
+            if(result == null){
+                console.log("this is null");
+            }
+            else{
+                console.log("find and fetch some items...");
+            }
+            db.close();
+            response.send(result);
+        });
+    });
+});
+
+//4. The front end want to get a user information.
 app.post("/getUser", function (request, response) {
     MongoClient.connect(url, function(err,db) {
         if (err) throw err;
-        var myobj = {name : request.body.name};
+        var myobj = {username : request.body.username};
         db.collection("users").findOne(myobj, function(err, result) {
             if (err) throw err;
             console.log(result);
@@ -44,7 +72,48 @@ app.post("/getUser", function (request, response) {
     });
 });
 
+//5. get daily challenge.
+app.post("/getChallenge", function (request, response) {
+    MongoClient.connect(url, function(err,db) {
+        if (err) throw err;
+        var myobj = {date : request.body.date};
+        db.collection("Challenge").findOne(myobj, function(err, result) {
+            if (err) throw err;
 
+            db.close();
+            response.send(result);
+        });
+    });
+});
+
+//6. get upcoming challenges.
+app.post("/getChallenge", function (request, response) {
+    MongoClient.connect(url, function(err,db) {
+        if (err) throw err;
+        var myobj = {date: {$in: ["10-20-2017", "10-21-2017", "10-22-2017", "10-23-2017"]}};
+        db.collection("Challenge").find(myobj).toArray(function(err, result) {
+            if (err) throw err;
+
+            db.close();
+            response.send(result);
+        });
+    });
+});
+
+//7. Check whether this user is approved for today's challenge
+app.post("/isApproved", function (request, response) {
+    MongoClient.connect(url, function(err,db) {
+        if (err) throw err;
+        var myobj = {date: response.body.date, username: request.body.username};
+        db.collection("Approved").find(myobj).toArray(function(err, result) {
+            if (err) throw err;
+            db.close();
+            response.send(result);
+        });
+    });
+});
+
+//start to listen port 8888
 app.listen(8888);
 
 
@@ -54,21 +123,22 @@ function createCollection(TableName){
       if (err) throw err;
       db.createCollection(TableName, function(err, res) {
         if (err) throw err;
-        console.log("create users table");
+        console.log("create a table called " + TableName);
         db.close();
       });
     });
 }
 
 //Insert one attribute to a collection
-function insertItem(username, password, nickname, email, phone, starttime, score, paired, pictureconfirmed, invitation){
+function insertItem(username, password, nickname, email, phone, score){
     MongoClient.connect(url, function(err,db) {
        if (err) throw err;
-        var myobj = {username : username, password: password, nickname : nickname, email : email, phone : phone, starttime: starttime, score: score, paired: paired, pictureconfirmed: pictureconfirmed, invitation: invitation };
+        var myobj = {username : username, password: password, nickname : nickname, email : email, phone : phone, score: score};
         db.collection("users").insertOne(myobj, function(err,res) {
             if (err) throw err;
             console.log("Insert 1 element correctly");
             db.close();
+            return res;
         });
     });
 }
@@ -84,6 +154,24 @@ function findItem(username){
         }
         db.close();
       });
+    });
+}
+
+//find all records that satisfy our search query.
+function findChallengers(username){
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        db.collection("Challenger").find({username : username}, function(err, result) {
+            if (err) throw err;
+            if(result == null){
+                console.log("this is null");
+            }
+            else{
+                console.log("find and fetch some items...");
+            }
+            db.close();
+            return result;
+        });
     });
 }
 
